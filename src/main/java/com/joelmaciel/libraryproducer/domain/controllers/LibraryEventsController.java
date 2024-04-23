d10bc75313a799f1b85ab59099aab7da161ce9b7
Joel <joel.viana@audora.com.br>
2024-04-23 09:48:04 -0300

    feat: configuring message produce.

---
+++ src/main/java/com/joelmaciel/libraryproducer/domain/controllers/LibraryEventsController.java
@@ -0,0 +1,34 @@
+package com.joelmaciel.libraryproducer.domain.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.joelmaciel.libraryproducer.domain.events.LibraryEvent;
+import com.joelmaciel.libraryproducer.producer.LibraryEventsProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+@RestController
+@Slf4j
+public class LibraryEventsController {
+
+    private final LibraryEventsProducer libraryEventsProducer;
+
+    public LibraryEventsController(LibraryEventsProducer libraryEventsProducer) {
+        this.libraryEventsProducer = libraryEventsProducer;
+    }
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        log.info("libraryEvent : {}", libraryEvent);
+        //libraryEventsProducer.sendLibraryEvent(libraryEvent);
+        libraryEventsProducer.sendLibraryEventApproachTwo(libraryEvent);
+        log.info("After Sending library event : ");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+}
---
+++ src/main/java/com/joelmaciel/libraryproducer/producer/LibraryEventsProducer.java
@@ -0,0 +1,69 @@
+package com.joelmaciel.libraryproducer.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joelmaciel.libraryproducer.domain.events.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Component
+@Slf4j
+public class LibraryEventsProducer {
+    @Value("${spring.kafka.topic}")
+    public String topic;
+
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public LibraryEventsProducer(KafkaTemplate<Integer, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        Integer key = libraryEvent.libraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        var completableFuture = kafkaTemplate.send(topic, key, value);
+
+        return completableFuture
+                .whenComplete((sendResult, throwable) -> {
+                    if (throwable != null) {
+                        handleFailure(key, value, throwable);
+                    } else {
+                        handleSuccess(key, value, sendResult);
+                    }
+                });
+    }
+
+    public SendResult<Integer, String> sendLibraryEventApproachTwo(LibraryEvent libraryEvent)
+            throws JsonProcessingException,
+                   ExecutionException,
+                   InterruptedException,
+                   TimeoutException
+    {
+        Integer key = libraryEvent.libraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        SendResult<Integer, String> sendResult = kafkaTemplate.send(topic, key, value).get(3, TimeUnit.SECONDS);
+        handleSuccess(key, value, sendResult);
+        return sendResult;
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
+        log.info("Message Send Successfully for the key : {} and the values: {} , partition is {} ",
+                key, value, sendResult.getRecordMetadata().partition());
+    }
+
+    private void handleFailure(Integer key, String value, Throwable throwable) {
+        log.error("Error sending the message and the exception is {} ", throwable.getMessage(), throwable);
+    }
+}
