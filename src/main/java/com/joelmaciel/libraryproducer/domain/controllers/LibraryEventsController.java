17850863bfc8e71858c64beea126d33af35b670f
Joel <joel.viana@audora.com.br>
2024-04-24 08:34:21 -0300

    feat: other ways to produce messages

--- src/main/java/com/joelmaciel/libraryproducer/domain/controllers/LibraryEventsController.java
+++ src/main/java/com/joelmaciel/libraryproducer/domain/controllers/LibraryEventsController.java
@@ -26,8 +26,9 @@ public class LibraryEventsController {
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
         log.info("libraryEvent : {}", libraryEvent);
-        //libraryEventsProducer.sendLibraryEvent(libraryEvent);
-        libraryEventsProducer.sendLibraryEventApproachTwo(libraryEvent);
+        // libraryEventsProducer.sendLibraryEvent(libraryEvent);
+        //  libraryEventsProducer.sendLibraryEventApproachTwo(libraryEvent);
+        libraryEventsProducer.sendLibraryEventApproachThree(libraryEvent);
         log.info("After Sending library event : ");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
