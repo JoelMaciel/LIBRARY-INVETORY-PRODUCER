2dc2350be996c151707972c697949e29ca5f1b42
Joel <joel.viana@audora.com.br>
2024-04-26 23:36:16 -0300

    feat: implementing  endpoint of update

--- src/main/java/com/joelmaciel/libraryproducer/api/controllers/LibraryEventsController.java
+++ src/main/java/com/joelmaciel/libraryproducer/api/controllers/LibraryEventsController.java
@@ -1,6 +1,7 @@
 package com.joelmaciel.libraryproducer.api.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.joelmaciel.libraryproducer.domain.enums.LibraryEventType;
 import com.joelmaciel.libraryproducer.domain.events.LibraryEvent;
 import com.joelmaciel.libraryproducer.producer.LibraryEventsProducer;
 import jakarta.validation.Valid;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,12 +27,32 @@ public class LibraryEventsController {
     }
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+    public ResponseEntity<LibraryEvent> createLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
         log.info("libraryEvent : {}", libraryEvent);
-        // libraryEventsProducer.sendLibraryEvent(libraryEvent);
-        //  libraryEventsProducer.sendLibraryEventApproachTwo(libraryEvent);
+
         libraryEventsProducer.sendLibraryEventApproachThree(libraryEvent);
-        log.info("After Sending library event : ");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> updateLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        log.info("libraryEvent : {}", libraryEvent);
+
+        ResponseEntity<String> BAD_REQUEST = validate(libraryEvent);
+        if (BAD_REQUEST != null) return BAD_REQUEST;
+
+        libraryEventsProducer.sendLibraryEventApproachThree(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
+
+    private static ResponseEntity<String> validate(LibraryEvent libraryEvent) {
+        if (libraryEvent.libraryEventId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the libraryEventId   ");
+        }
+
+        if(!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE event type is supported   ");
+        }
+        return null;
     }
 }
